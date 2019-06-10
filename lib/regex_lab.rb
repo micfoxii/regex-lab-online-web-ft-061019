70c87775b0fb6253ef4195952c56ab85677cadf8
Michael Fox <michael.c.fox.ii@gmail.com>
2019-06-10 21:14:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ end
 
 def words_five_letters_long(text)
   words = text.split(" ")
-  words.grep(/\b\w{5}\b/)
+  $new_array.push words.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
